8491a5e217dd73d3f2c840d54ec8f1cbbdfdbfe9
rupaparabhumi <rupaparabhumi8@gmail.com>
2024-09-21 17:06:53 +0530

    Update Answer3-5.c

--- Answer3-5.c
+++ Answer3-5.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
- int i,fact=1,number;
+ int i,n,fact=0;
 
  printf("Enter a number: ");
-  scanf("%d",&number);
+  scanf("%d",&n);
 
-    for(i=1;i<=number;i++)
+    for(i=1; i<=n; i++)
   {
      fact*=i;
   }
-  printf("Factorial of %d is: %d",number,fact);
+  printf("Factorial of %d is: %d",n,fact);
 
   return 0;
 }
