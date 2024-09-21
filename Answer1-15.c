bdeeac16737a91dbf2e745c4d496f07b544ee930
rupaparabhumi <rupaparabhumi8@gmail.com>
2024-09-21 17:10:51 +0530

    Update Answer1-15.c

--- Answer1-15.c
+++ Answer1-15.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int main() {
 
-  char fname[20], mname[20], lname[20];
+  char name[50];
 
-  printf("Enter The School name:  ");
-  scanf("%s %s %s", fname, mname, lname);
+  printf("Enter The School name:");
+  scanf("%s %s %s",&name,&name,&name);
 
-  printf("Abbreviated Name: ");
-  printf("%c. %c. %s\n", fname[0], mname[0], lname);
+  printf("Abbreviated Name:");
+  printf("%c.%c.%s\n",name[0],name[0],name);
+
   return 0;
 
 }
+
