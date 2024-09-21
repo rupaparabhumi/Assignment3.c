2234a0b63bf96ee26a58fbd4368b098a45de3c93
rupaparabhumi <rupaparabhumi8@gmail.com>
2024-09-21 17:11:42 +0530

    Update Answer1-12.c

--- Answer1-12.c
+++ Answer1-12.c
@@ -2,16 +2,14 @@
 
 int main (){
 
-int student,apples;
+float student,apple=5,Totalapple;
 
 printf("Enter your student:");
-scanf("%d",&student);
+scanf("%f",&student);
 
-printf("Enter your apples:");
-scanf("%d",&apples);
+Totalapple=student/(float)5;
 
-float divResult=student/(float)apples;
-printf ("\nDivision of %d and %d is :%.2f",student,apples,divResult);
+printf ("\nTotal apple :%.2f",Totalapple);
 
 return (0);
 
